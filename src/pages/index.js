8e7e4d7fb7c3bd56b189edb5f8160211efa5dd57
cicingik <danysatyanegara@gmail.com>
2023-02-05 15:25:31 +0700

    fix: import typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import Helmet from 'react-helmet'
 import config from '../utils/config'
 
-import { Layout } from '../components/layout'
+import { Layout } from '../components/Layout'
 import { Hero } from '../components/Hero'
 import { Heading } from '../components/Heading'
 
