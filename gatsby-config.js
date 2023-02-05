c2681ca614413900e9ba99483f264ecb806dd217
cicingik <danysatyanegara@gmail.com>
2023-02-05 12:32:41 +0700

    fix: gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,67 +32,6 @@ module.exports = {
         icon: `static/logo.png`,
       },
     },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map((edge) => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [
-                    { 'content:encoded': edge.node.html },
-                    { author: 'hello@cicingik.com' },
-                  ],
-                })
-              })
-            },
-            query: `
-              {
-                allMarkdownRemark(
-                  limit: 30,
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                  filter: { frontmatter: { template: { eq: "post" } } }
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      fields { 
-                        slug 
-                      }
-                      frontmatter {
-                        title
-                        date
-                        template
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: '/rss.xml',
-            title: 'cicingik | RSS Feed',
-          },
-        ],
-      },
-    },
 
     // ===================================================================================
     // Images and static
