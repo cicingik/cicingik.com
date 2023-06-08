b6d079fc553a1265e92bfe6c114ad639433e7ef8
dany satyanegara <47546703+cicingik@users.noreply.github.com>
2023-06-08 09:16:35 +0700

    feat: new themes (#1)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,93 +1,29 @@
 module.exports = {
-  siteMetadata: {
-    title: 'cicingik',
-    author: {
-      name: 'cicingik',
-    },
-    pathPrefix: '/',
-    siteUrl: 'https://www.cicingik.com',
-    description:
-      'Software engineer and open-source creator. This is my digital garden.',
-    feedUrl: 'https://www.cicingik.com/rss.xml',
-    logo: 'https://www.cicingik.com/logo.png',
-  },
   plugins: [
-    // ===================================================================================
-    // Meta
-    // ===================================================================================
-
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-netlify',
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        name: 'cicingik',
-        short_name: 'cicingik',
-        description:
-          'Software engineer and open source creator. This is my digital garden.',
-        start_url: '/',
-        background_color: 'white',
-        theme_color: '#6b76f3',
-        display: 'minimal-ui',
-        icon: `static/logo.png`,
-      },
-    },
-
-    // ===================================================================================
-    // Images and static
-    // ===================================================================================
-
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'posts',
-        path: `${__dirname}/content/`,
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'assets',
-        path: `${__dirname}/static/`,
-      },
-    },
-
-    // ===================================================================================
-    // Markdown
-    // ===================================================================================
-
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          'gatsby-remark-autolink-headers',
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 800,
-              // linkImagesToOriginal: false,
-              backgroundColor: 'transparent',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-prismjs',
-            options: {
-              classPrefix: 'language-',
-              inlineCodeMarker: null,
-              aliases: {},
-              showLineNumbers: false,
-              noInlineHighlight: false,
-              prompt: {
-                user: 'root',
-                host: 'localhost',
-                global: true,
-              },
-            },
-          },
-        ],
+        siteUrl: "https://cicingik.com", // Used for sitemap generation
+        manifestSettings: {
+          favicon: "./content/images/favicon.png", // Path is relative to the root
+          siteName: "cicingik", // Used in manifest.json
+          shortName: "cicingik", // Used in manifest.json
+          startUrl: "/", // Used in manifest.json
+          backgroundColor: "#FFFFFF", // Used in manifest.json
+          themeColor: "#000000", // Used in manifest.json
+          display: "minimal-ui", // Used in manifest.json
+        },
+        contentDirectory: "./content",
+        blogSettings: {
+          path: "/blog", // Defines the slug for the blog listing page
+          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+        },
+        // googleAnalytics: {
+        //     trackingId: "UA-XXXXXX-X",
+        //     anonymize: true, // Default true
+        //     environments: ["production", "development"] // Default ["production"]
+        // }
       },
     },
   ],
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,25 @@
-import * as React from 'react'
-import Helmet from 'react-helmet'
-import config from '../utils/config'
+import React from "react";
+import {
+  AboutSection,
+  ArticlesSection,
+  ContactSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
 
-import { Layout } from '../components/Layout'
-import { Hero } from '../components/Hero'
-import { Heading } from '../components/Heading'
-
-
-export default function Index() {
-    return (
-        <div>
-            <Helmet title={config.siteTitle}/>
-
-            <div className="container">
-                <div className="hero-wrapper">
-                    <Hero title="Hey, I'm cicingik!" index>
-                        <p className="hero-description small width">
-                            Welcome to my digital garden. 🌱
-                            <br/>
-                            <br/>
-                            I'm a software developer in Chicago. I make
-                            open-source projects and write about code, design, and life. I like
-                            accordions, drawing, sci-fi, reading, and gaming.
-                        </p>
-                        <br/>
-                        <p>
-                            <small className="text-muted">
-                                <em>Last updated: February 5th, 2023</em>
-                            </small>
-                        </p>
-                    </Hero>
-                    <div className="decoration">
-                        <img
-                            src="/millenium-falcon.png"
-                            alt="RAM Ram"
-                            className="image hero-image"
-                            title="RAM Ram"
-                        />
-                    </div>
-                </div>
-            </div>
-
-            <div className="container">
-
-                <section className="segment large">
-                    <Heading title="Contact" />
-                    <p>
-                        Reach me on email.
-                    </p>
-                    <p>
-                        <a
-                            href="mailto:danysatyanegara@gmail.com"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="button large highlighted"
-                        >
-                            Sent Email
-                        </a>
-                    </p>
-                </section>
-            </div>
-
-        </div>
-    )
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="cicingik.com" />
+      <Page>
+        <HeroSection sectionId="hero" />
+        <AboutSection sectionId="about" heading="About Me" />
+        <InterestsSection sectionId="interest" heading="Interest" />
+        <ContactSection sectionId="contact" heading="Contact" />
+      </Page>
+    </>
+  );
 }
-
-Index.Layout = Layout
